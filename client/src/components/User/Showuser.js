6407da01efee933b27b8bd26d63b50a6c57d72c7
Ankush Sinha <97420654+ankushSinha1@users.noreply.github.com>
2023-05-25 10:24:35 +0530

    Show user ui improved

--- client/src/components/User/Showuser.js
+++ client/src/components/User/Showuser.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import { Link, useNavigate, useParams } from 'react-router-dom';
+import { notify } from '../CustomStyling/notify';
 // import { Navbar } from '../Navbar/Navbar.js';
 
 
